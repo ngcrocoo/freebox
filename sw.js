81c65d2db2133127f6f40bcf5882a1930554ffb5
S-Bartsch <Sabeth.Bartsch@gmail.com>
2022-05-11 14:58:45 +0200

    favicon

---
+++ sw.js
@@ -0,0 +1,17 @@
+// installing service worker. 
+// install works just once for every browser and will be installed again
+// if this file is changed
+self.addEventListener('install', evt => {
+    console.log('service worker has been installed');
+});
+
+// activate service worker
+self.addEventListener('activate', evt => {
+    console.log('service worker has been activated');
+});
+
+
+// fetch event
+self.addEventListener('fetch', evt => {
+    conslotchange.log('fetch event', evt);
+});
